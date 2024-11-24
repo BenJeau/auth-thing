e191e2c198ab6061030fb12b3d0200ea9ea19e61
Benoit Jeaurond <benoit@jeaurond.dev>
2024-11-24 13:09:17 -0500

    chore: add some password requirements

--- backend/database/migrations/0002_data.up.sql
+++ backend/database/migrations/0002_data.up.sql
@@ -3,9 +3,9 @@ VALUES
   ('1', 'admin@admin.com', 'John Doe', true, null, null),
   ('2', 'user@user.com', 'Jane Doe', false, '$argon2id$v=19$m=19456,t=2,p=1$ylgx6jpeHPpbgGaq0UT/AQ$/NVMxZXJX6O+SFNZEt78i5FJYlvX2i7yElOvx4aRdwo', DATETIME('now')); --- Code: 12345678
 
-INSERT INTO applications (id, slug, name, description, website, creator_id)
+INSERT INTO applications (id, slug, name, description, website, creator_id, password_min_length, password_min_strength)
 VALUES
-  ('1', 'example', 'Example', 'Example application', 'https://example.com', '1');
+  ('1', 'example', 'Example', 'Example application', 'https://example.com', '1', 8, 'medium');
 
 INSERT INTO application_passwords (application_id, user_id, password)
 VALUES
